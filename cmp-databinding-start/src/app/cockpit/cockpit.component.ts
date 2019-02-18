4e06eb9adb2e9292d794492d97fd06a266a40864
juanferrgiraldo <juanferrgiraldo@gmail.com>
2019-02-18 17:37:09 -0500

    M5: local references with viewChild

--- cmp-databinding-start/src/app/cockpit/cockpit.component.ts
+++ cmp-databinding-start/src/app/cockpit/cockpit.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-cockpit',
@@ -8,8 +8,9 @@ import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 export class CockpitComponent implements OnInit {
   @Output() serverCreated = new EventEmitter<{serverName: string, serverContent: string}>();
   @Output() blueprintCreated = new EventEmitter<{serverName: string, serverContent: string}>();
-  newServername = '';
-  newServerContent = '';
+  // newServername = '';
+  // newServerContent = '';
+  @ViewChild('serverContentInput') serverContentInput: ElementRef; // This is a local reference fetch throw ViewChild()
 
   constructor() { }
 
@@ -18,15 +19,15 @@ export class CockpitComponent implements OnInit {
 
   onAddServer(nameInput: HTMLInputElement) { // This method triggers the onServerAdded() method in app.component.ts file thanks to Output and EventEmitter properties
     this.serverCreated.emit({
-      serverName: nameInput.value,
-      serverContent: this.newServerContent
+      serverName: nameInput.value, // This is a local reference passed by the method
+      serverContent: this.serverContentInput.nativeElement.value
     })
   }
 
   onAddBlueprint(nameInput: HTMLInputElement) {
     this.blueprintCreated.emit({
       serverName: nameInput.value,
-      serverContent: this.newServerContent
+      serverContent: this.serverContentInput.nativeElement.value
     })
   }
 
