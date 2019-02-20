30f31ef932ed585400bf56defcd6646ad7584f23
juanferrgiraldo <juanferrgiraldo@gmail.com>
2019-02-20 01:23:33 -0500

    Passing ingredients throw components using services

--- shopping-recipe/src/app/recipes/recipe.service.ts
+++ shopping-recipe/src/app/recipes/recipe.service.ts
@@ -1,15 +1,37 @@
-import { EventEmitter } from '@angular/core';
+import { EventEmitter, Injectable } from '@angular/core';
 
 import { Recipe } from './recipe.model';
+import { Ingredient } from '../shared/ingredient.model';
+import { ShoppingListService } from '../shopping-list/shopping-list.service';
 
+@Injectable()
 export class RecipeService {
   recipeSelected = new EventEmitter<Recipe>();
+
   private recipes: Recipe[] = [
-    new Recipe('A test recipe', 'This is a test recipe', 'https://get.pxhere.com/photo/dish-meal-food-produce-recipe-fish-breakfast-meat-pork-cuisine-steak-pork-chop-power-dishes-grilling-fried-food-604134.jpg'),
-    new Recipe('Another test recipe', 'This is another test recipe', 'https://get.pxhere.com/photo/dish-meal-food-produce-recipe-fish-breakfast-meat-pork-cuisine-steak-pork-chop-power-dishes-grilling-fried-food-604134.jpg'),
+    new Recipe('A test recipe', 
+      'This is a test recipe', 
+      'https://get.pxhere.com/photo/dish-meal-food-produce-recipe-fish-breakfast-meat-pork-cuisine-steak-pork-chop-power-dishes-grilling-fried-food-604134.jpg',
+      [
+        new Ingredient('Meat', 1),
+        new Ingredient('French fries', 20)
+      ]),
+    new Recipe('Another test recipe', 
+      'This is another test recipe', 
+      'https://get.pxhere.com/photo/dish-meal-food-produce-recipe-fish-breakfast-meat-pork-cuisine-steak-pork-chop-power-dishes-grilling-fried-food-604134.jpg',
+      [
+        new Ingredient('Bonus', 2),
+        new Ingredient('Meat', 1)
+      ]),
   ]
+
+  constructor(private shoppingListService: ShoppingListService) { }
 
   getRecipes() {
     return this.recipes.slice(); // The array is being returned by reference, so slice is used to get copy of the array
+  }
+
+  onAddIngredientsToShoppingList(ingredients: Ingredient[]) {
+    this.shoppingListService.addIngredients(ingredients);
   }
 }
--- shopping-recipe/src/app/shopping-list/shopping-list.service.ts
+++ shopping-recipe/src/app/shopping-list/shopping-list.service.ts
@@ -1,3 +1,25 @@
+import { Ingredient } from '../shared/ingredient.model';
+import { EventEmitter } from '@angular/core';
+
 export class ShoppingListService {
-  
+  ingredientsChanged = new EventEmitter<Ingredient[]>(); // This event is used to get informed that the ingredient list has changed, so is needed to make a new copy
+
+  private ingredients: Ingredient[] = [
+    new Ingredient('Apples', 5),
+    new Ingredient('Tomatoes', 10)
+  ];
+
+  getIngredients() {
+    return this.ingredients.slice();
+  }
+
+  addIngredient(ingredient: Ingredient) {
+    this.ingredients.push(ingredient);
+    this.ingredientsChanged.emit(this.ingredients.slice());  // Crate a new copy of shopping list
+  }
+
+  addIngredients(ingredients: Ingredient[]) {
+    this.ingredients.push(...ingredients);
+    this.ingredientsChanged.emit(this.ingredients.slice());
+  }
 }
