f4730a255436e48df120f98ff650cfcb380723da
juanferrgiraldo <juanferrgiraldo@gmail.com>
2019-02-25 21:54:23 -0500

    M14: changing event emitter to subject for cross component communication

--- shopping-recipe/src/app/shopping-list/shopping-list.component.ts
+++ shopping-recipe/src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
 
 import { Ingredient } from '../shared/ingredient.model';
 import { ShoppingListService } from './shopping-list.service';
@@ -8,15 +9,21 @@ import { ShoppingListService } from './shopping-list.service';
   templateUrl: './shopping-list.component.html',
   styleUrls: ['./shopping-list.component.css']
 })
-export class ShoppingListComponent implements OnInit {
+export class ShoppingListComponent implements OnInit, OnDestroy {
   ingredients: Ingredient[];
+  private subscription: Subscription;
+
   constructor(private shoppingListService: ShoppingListService) { }
 
   ngOnInit() {
     this.ingredients = this.shoppingListService.getIngredients();
-    this.shoppingListService.ingredientsChanged.subscribe( // Subcribe to an event to get noticed when the shopping list change
+    this.subscription = this.shoppingListService.ingredientsChanged.subscribe( // Subcribe to an event to get noticed when the shopping list change
       (ingredients: Ingredient[]) => {
         this.ingredients = ingredients;
       });
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
   }
 }
--- shopping-recipe/src/app/shopping-list/shopping-list.service.ts
+++ shopping-recipe/src/app/shopping-list/shopping-list.service.ts
@@ -1,8 +1,8 @@
 import { Ingredient } from '../shared/ingredient.model';
-import { EventEmitter } from '@angular/core';
+import { Subject } from 'rxjs';
 
 export class ShoppingListService {
-  ingredientsChanged = new EventEmitter<Ingredient[]>(); // This event is used to get informed that the ingredient list has changed, so is needed to make a new copy
+  ingredientsChanged = new Subject<Ingredient[]>(); // This event is used to get informed that the ingredient list has changed, so is needed to make a new copy
 
   private ingredients: Ingredient[] = [
     new Ingredient('Apples', 5),
@@ -15,11 +15,11 @@ export class ShoppingListService {
 
   addIngredient(ingredient: Ingredient) {
     this.ingredients.push(ingredient);
-    this.ingredientsChanged.emit(this.ingredients.slice());  // Crate a new copy of shopping list
+    this.ingredientsChanged.next(this.ingredients.slice());  // Crate a new copy of shopping list
   }
 
   addIngredients(ingredients: Ingredient[]) {
     this.ingredients.push(...ingredients);
-    this.ingredientsChanged.emit(this.ingredients.slice());
+    this.ingredientsChanged.next(this.ingredients.slice());
   }
 }
